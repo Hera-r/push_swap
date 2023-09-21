5c01b1eddff44474bbed0241953e20f337206a2b
Hera.r <tennyhera.r@gmail.com>
2023-09-21 23:01:28 +0200

    basic_sort

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hrandria <hrandria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 18:20:44 by hrandria          #+#    #+#             */
-/*   Updated: 2023/09/17 01:18:48 by hrandria         ###   ########.fr       */
+/*   Updated: 2023/09/21 23:01:07 by hrandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,6 @@ typedef struct s_list
 	int			*value;
 	struct s_list	*next;
 }					t_list;
-
-typedef struct s_pos
-{
-	int			min;
-	int			index;
-}					t_posi;
 
 int	ft_list_size(t_list *array)
 {
@@ -40,41 +34,21 @@ int	ft_list_size(t_list *array)
 	return (size);
 }
 
-t_posi	ft_nb_min(t_list *array)
+int	ft_nb_min(t_list *array)
 {
 	t_list *tmp;
-	t_list *tmp2;
-
-	t_posi data;
 	int	min;
-	int	index;
 
-	index = 0;
 	tmp = array;
-	tmp2 = array;
 	min = *tmp->value;
-
 	while (tmp != NULL)
 	{
 		if (*tmp->value < min)
 			min = *tmp->value;
 		tmp = tmp->next;
 	}
-	while (tmp2 != NULL)
-	{
-		if (*tmp2->value == min)
-		{
-			index++;
-			break;
-		}
-		tmp2 = tmp2->next;
-		index++;
-	}
-	data.min = min;
-	data.index = index;
-	return (data);
+	return (min);
 }
-
 
 int	ft_is_sorting(t_list *array)
 {
@@ -171,7 +145,7 @@ t_list	*ft_lstappend(t_list *array, int *value)
 	t_list	*element;
 	t_list	*tmp;
 
-	tmp = array;	
+	tmp = array;
 	element = malloc(sizeof(*element));
 	if (element == NULL)
 		return (NULL);
@@ -188,6 +162,41 @@ t_list	*ft_lstappend(t_list *array, int *value)
 	return (array);
 }
 
+// t_list	*sort_list_in_ascending_order(t_list *array)
+// {
+// 	t_list	*list_asc;
+// 	t_list	*tmp;
+
+// 	tmp = array;
+// 	while (tmp!= NULL)
+// 	{
+// 		list_asc = ft_lstappend(list_asc, *tmp->value);
+// 		tmp = tmp->next;
+// 	}
+
+// }
+
+// {1, 2, 11, 8, 7, 9, 5, 3, 6, 10}
+void	ft_basic_sort(t_list **array)
+{
+	t_list	*lstmp;
+	int		*tmp;
+
+	lstmp = *array;
+	while (lstmp->next != NULL)
+	{
+		if (*lstmp->value > *lstmp->next->value)
+		{
+			tmp = lstmp->next->value;
+			lstmp->next->value = lstmp->value;
+			lstmp->value = tmp;
+		}
+		lstmp = lstmp->next;
+		if (lstmp->next == NULL && ft_is_sorting(*array) != 0)
+			lstmp = *array;
+	}
+}
+
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -196,7 +205,6 @@ int	main(void)
 {
 	t_list *my_list;
 	t_list *list_b;
-	t_posi data;
 
 	srand(time(NULL));
 	int	size = 0;
@@ -204,24 +212,9 @@ int	main(void)
 	my_list = NULL;
 	list_b = NULL;
 
-	// int a = 3;
-	// int b = 2;
-	// int c = 7;
-	// int d = 0;
-	// // int d = 9;
-
-	// my_list = ft_lstappend(my_list, &a);
-	// my_list = ft_lstappend(my_list, &b);
-	// my_list = ft_lstappend(my_list, &c);
-	// my_list = ft_lstappend(my_list, &d);
 	
 	// {1,2, 3, 4, 5, 6, 7, 8}
-	int randomNumber[] = {986, 682, 121, 743, 226, 759, 827, 455,
-	 788, 290, 888, 37, 241, 466, 618, 684, 891, 815, 923, 
-	 485, 301, 896, 27, 975, 769, 735, 594, 990, 53, 575, 361,
-	  175, 426, 260, 625, 207, 488, 333, 434, 638, 864, 699, 197, 
-	  521, 411, 315, 753, 984, 218, 541, 149, 33, 951, 610, 19, 102, 993, 721, 357, 35, 131, 912, 373, 718, 886, 832, 876, 162, 665, 240, 906, 387, 608, 559, 268, 120, 467, 139, 758, 22, 634, 251, 42, 879, 337, 283, 346, 407, 664, 825, 772, 670, 767, 104, 93, 648, 46, 582, 502, 846, 939, 748, 861, 430, 454, 809, 545, 617, 655, 608, 171, 23, 689, 605, 522, 501, 696, 285, 179, 882, 66, 25, 749, 729, 570, 752, 44, 220, 781, 973, 875, 406, 445, 92, 103, 30, 872, 324, 738, 848, 67, 461, 107, 75, 860, 865, 482, 432, 458, 140, 930, 331, 415, 802, 706, 547, 157, 195, 736, 259, 161, 789, 216, 892, 996, 598, 370, 124, 615, 580, 151, 867, 368, 493, 229, 720, 299, 322, 657, 900, 531, 258, 732, 870, 70, 969, 593, 800, 709, 583, 273, 754, 91, 782, 221, 738, 184, 189, 906, 508, 104, 265, 937, 227, 153, 156, 377, 854, 971, 471, 94, 472, 784, 491, 745, 790, 39, 77, 940, 729, 458, 694, 275, 741, 991, 351, 888, 34, 320, 562, 71, 194, 581, 330, 933, 894, 224, 885, 947, 478, 716, 424, 765, 388, 15, 898, 606, 591, 685, 116, 579, 622, 172, 208, 838, 530, 286, 512, 98, 429, 376, 506, 242, 928, 539, 31, 711, 248, 960, 678, 438, 893, 400, 269, 465, 859, 236, 311, 270, 288, 980, 306, 334, 852, 631, 858, 268, 558, 683, 468, 826, 166, 582, 746, 513, 187, 95, 777, 968, 
-	972, 713, 433, 761, 587, 239, 914, 58, 509, 328, 122, 109, 881}; //{1, 2, 11, 8, 7, 9, 5, 3, 6, 10};
+	int randomNumber[] = {11, 2, 1, 8, 7, 9, 5, 3, 6, 10};
 	int taille = sizeof(randomNumber) / sizeof(randomNumber[0]);
     // Initialiser le générateur de nombres aléatoires avec le temps actuel
     srand(time(NULL));
@@ -236,68 +229,19 @@ int	main(void)
     }
 
     for (int i = 0; i < taille; i++) {
+		printf("app - %d\n", randomNumber[i]);
         my_list = ft_lstappend(my_list, &randomNumber[i]);
     }
 
-	int count = 0;
-	while (ft_list_size(my_list) != 1)
-	{
-		// {2, 1, 11, 7, 4}
-		// printf("tmp --- %d\n", tmp);
-		int size_lsit = ft_list_size(my_list);
-		int half_list = size_lsit  / 2;
-		int	half = size_lsit  / 2;
-		data = ft_nb_min(my_list);
-		if (data.index <= half)
-		{
-			if (data.min != *my_list->value)
-			{
-				while (data.min != *my_list->value)
-				{
-					ft_ra_rotate(&my_list);
-					count++;
-				}
-				ft_pb_push(&my_list, &list_b);
-				count++;
-				data = ft_nb_min(my_list);
-			}
-			else
-			{
-			ft_pb_push(&my_list, &list_b);
-			count++;
-			data = ft_nb_min(my_list);
-			}
-		}
-		if (data.index > half)
-		{
-			if (data.min != *my_list->value)
-			{
-				while (data.min != *my_list->value)
-				{
-					ft_rra_rotate(&my_list);
-				count++;
-				}
-				ft_pb_push(&my_list, &list_b);
-				count++;
-				data = ft_nb_min(my_list);
-			}
-			else
-			{
-				ft_pb_push(&my_list, &list_b);
-				count++;
-				data = ft_nb_min(my_list);
-			}
-		}
-	}
-	ft_pb_push(&my_list, &list_b);
-	count++;
-	// t_list *tmp = my_list;
+	ft_basic_sort(&my_list);
 
-	// while (tmp != NULL)
-	// {
-	// 	printf("%d\n", *tmp->value);
-	// 	tmp = tmp->next;
-	// }
+	t_list *tmp = my_list;
+
+	while (tmp != NULL)
+	{
+		printf("%d\n", *tmp->value);
+		tmp = tmp->next;
+	}
 	// printf("**************************\n");
 	// ft_ra_rotate(&my_list);
 	// ft_sa_swap(&my_list);
@@ -324,12 +268,11 @@ int	main(void)
 	// min = ft_nb_min(my_list);
 	// printf("Nb-Min: %d\n", min);
 
-	while (list_b != NULL)
-	{
-		printf("+++++=> %d\n", *list_b->value);
-		list_b = list_b->next;
-	}
-	printf("nb coup %d\n", count);
+	// while (list_b != NULL)
+	// {
+	// 	printf("+++++=> %d\n", *list_b->value);
+	// 	list_b = list_b->next;
+	// }
 	// return (0);
 	// while (my_list != NULL)
 	// {
@@ -367,3 +310,4 @@ rra: Le dernier élément devient le premier.
 		- manao fonction mijery hoe inférieur sa supérieur
 
 */
+
